8c7e8737ef95d73c39d1b1bd07fa7f6741250c00
Mehran Hydary <mehran.hydary@gmail.com>
2020-01-04 17:26:29 -0500

    simple lottery smart contract added

--- server/src/blockchain/contracts/Lottery.sol
+++ server/src/blockchain/contracts/Lottery.sol
@@ -2,18 +2,32 @@ pragma solidity ^0.5.0;
 
 contract Lottery {
     address public manager;
-    address[] public players;
+    address payable[] public players;
     
     constructor() public {
         manager = msg.sender;
     }
     
     function enter() public payable {
-        require(msg.value > 0.1 ether);
+        require(msg.value > 0.1 ether, "Please send more than 0.1 ether to this contract");
         players.push(msg.sender);
     }
     
-    function random() public view returns (uint) {
-        return uint(keccak256(abi.encodePacked(block.difficulty, now, players)));
+    function random() private view returns (uint) {
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
+    }
+    
+    function pickWinner() public restricted {
+        uint index = random() % players.length;
+        players[index].transfer(address(this).balance);
+        players = new address payable[](0);
+    }
+    function getPlayers() public view returns(address payable[] memory) {
+        return players;
+    }
+    
+    modifier restricted() {
+        require(msg.sender == manager, "Only the manager of this lottery can call this function");
+        _;
     }
 }
